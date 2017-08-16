82e4104b0660fac0d9f7aefc02789237ec331c1b
alec-hs <alec-hs@users.noreply.github.com>
2017-08-17 00:17:38 +0200

    Added additional conditions.
    Added extra conditions to the actions to make sure you can only access the actions from the correct seats inside the vehicle.

--- source/van-door-actions/soft_f_vda/config.cpp
+++ source/van-door-actions/soft_f_vda/config.cpp
@@ -35,7 +35,7 @@ class CfgVehicles {
 				userActionID = 53;
 				displayName = "Open Driver Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_1_source' == 0) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2)";
+				condition = "(this animationPhase 'Door_1_source' == 0) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2) && ((this getCargoIndex player) != 0)";
 				statement = "this animateDoor ['Door_1_source', 1,false];";
 			};
 
@@ -43,17 +43,16 @@ class CfgVehicles {
 				userActionID = 54;
 				displayName = "Close Driver Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_1_source' == 1) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2)";
+				condition = "(this animationPhase 'Door_1_source' == 1) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2) && ((this getCargoIndex player) != 0)";
 				statement = "this animateDoor ['Door_1_source', 0,false];";
 			};
 
-			/*Added  "&& (diver this != player)" to condition to check player is not in driver seat*/
 
 			class open_passenger_door: door_action {
 				userActionID = 55;
 				displayName = "Open Passenger Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_2_source' == 0) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2)  && (diver this != player)";
+				condition = "(this animationPhase 'Door_2_source' == 0) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2) && (driver this != player)";
 				statement = "this animateDoor ['Door_2_source', 1,false];";
 			};
 
@@ -61,7 +60,7 @@ class CfgVehicles {
 				userActionID = 56;
 				displayName = "Close Passenger Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_2_source' == 1) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2)  && (diver this != player)";
+				condition = "(this animationPhase 'Door_2_source' == 1) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2) && (driver this != player)";
 				statement = "this animateDoor ['Door_2_source', 0,false];";
 			};
 
@@ -69,7 +68,7 @@ class CfgVehicles {
 				userActionID = 57;
 				displayName = "Open Side Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_3_source' == 0) && ((player distance2D (this modelToWorld [1,1,-1])) < 2)  && (diver this != player)";
+				condition = "(this animationPhase 'Door_3_source' == 0) && ((player distance2D (this modelToWorld [1,1,-1])) < 2) && (driver this != player) && ((this getCargoIndex player) != 0)";
 				statement = "this animateDoor ['Door_3_source', 1,false];";
 			};
 
@@ -77,7 +76,7 @@ class CfgVehicles {
 				userActionID = 58;
 				displayName = "Close Side Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_3_source' == 1) && ((player distance2D (this modelToWorld [1,1,-1])) < 2)  && (diver this != player)";
+				condition = "(this animationPhase 'Door_3_source' == 1) && ((player distance2D (this modelToWorld [1,1,-1])) < 2) && (driver this != player) && ((this getCargoIndex player) != 0)";
 				statement = "this animateDoor ['Door_3_source', 0,false];";
 			};
 
@@ -85,7 +84,7 @@ class CfgVehicles {
 				userActionID = 59;
 				displayName = "Open Back Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_4_source' == 0) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2)  && (diver this != player)";
+				condition = "(this animationPhase 'Door_4_source' == 0) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2)";
 				statement = "this animateDoor ['Door_4_source', 1,false];";
 			};
 
@@ -93,7 +92,7 @@ class CfgVehicles {
 				userActionID = 60;
 				displayName = "Close Back Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_4_source' == 1) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2)  && (diver this != player)";
+				condition = "(this animationPhase 'Door_4_source' == 1) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2)";
 				statement = "this animateDoor ['Door_4_source', 0,false];";
 			};
 		};
@@ -101,84 +100,85 @@ class CfgVehicles {
 
 	class Van_02_transport_base_F: Van_02_base_F {
 
-		class UserActions: UserActions {
+		class UserActions {
 
 			class door_action {
 				userActionID = 52;
-        displayName = "Open Door";
-        radius = 10;
-        available = 0;
-        priority = 0.5;
-        position = "";
-        showWindow = 0;
-        onlyForPlayer = 1;
-        shortcut = "";
-        condition = "false";
-        statement = "";
+				displayName = "Open Door";
+				radius = 10;
+				available = 0;
+				priority = 0.5;
+				position = "";
+				showWindow = 0;
+				onlyForPlayer = 1;
+				shortcut = "";
+				condition = "false";
+				statement = "";
 			};
 
 			class open_driver_door: door_action {
 				userActionID = 53;
 				displayName = "Open Driver Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_1_source' == 0) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2)";
-        statement = "this animateDoor ['Door_1_source', 1,false];";
+				condition = "(this animationPhase 'Door_1_source' == 0) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2) && ((this getCargoIndex player) != 0) && ((this getCargoIndex player) != 8)";
+				statement = "this animateDoor ['Door_1_source', 1,false];";
 			};
 
 			class close_driver_door: door_action {
 				userActionID = 54;
 				displayName = "Close Driver Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_1_source' == 1) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2)";
-        statement = "this animateDoor ['Door_1_source', 0,false];";
+				condition = "(this animationPhase 'Door_1_source' == 1) && ((player distance2D (this modelToWorld [-1,2.2,-1])) < 2) && ((this getCargoIndex player) != 0) && ((this getCargoIndex player) != 8)";
+				statement = "this animateDoor ['Door_1_source', 0,false];";
 			};
+
 
 			class open_passenger_door: door_action {
 				userActionID = 55;
 				displayName = "Open Passenger Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_2_source' == 0) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2)";
-        statement = "this animateDoor ['Door_2_source', 1,false];";
+				condition = "(this animationPhase 'Door_2_source' == 0) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2) && (driver this != player) && ((this getCargoIndex player) != 8)";
+				statement = "this animateDoor ['Door_2_source', 1,false];";
 			};
 
 			class close_passenger_door: door_action {
 				userActionID = 56;
 				displayName = "Close Passenger Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_2_source' == 1) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2)";
-        statement = "this animateDoor ['Door_2_source', 0,false];";
+				condition = "(this animationPhase 'Door_2_source' == 1) && ((player distance2D (this modelToWorld [1,2.2,-1])) < 2) && (driver this != player) && ((this getCargoIndex player) != 8)";
+				statement = "this animateDoor ['Door_2_source', 0,false];";
 			};
 
 			class open_side_door: door_action {
 				userActionID = 57;
 				displayName = "Open Side Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_3_source' == 0) && ((player distance2D (this modelToWorld [1,1,-1])) < 2)";
-        statement = "this animateDoor ['Door_3_source', 1,false];";
+				condition = "(this animationPhase 'Door_3_source' == 0) && ((player distance2D (this modelToWorld [1,1,-1])) < 2) && (driver this != player) && ((this getCargoIndex player) != 0) && ((this getCargoIndex player) != 8)";
+				statement = "this animateDoor ['Door_3_source', 1,false];";
 			};
 
 			class close_side_door: door_action {
 				userActionID = 58;
 				displayName = "Close Side Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_3_source' == 1) && ((player distance2D (this modelToWorld [1,1,-1])) < 2)";
-        statement = "this animateDoor ['Door_3_source', 0,false];";
+				condition = "(this animationPhase 'Door_3_source' == 1) && ((player distance2D (this modelToWorld [1,1,-1])) < 2) && (driver this != player) && ((this getCargoIndex player) != 0) && ((this getCargoIndex player) != 8)";
+				statement = "this animateDoor ['Door_3_source', 0,false];";
 			};
 
 			class open_back_door: door_action {
 				userActionID = 59;
 				displayName = "Open Back Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_4_source' == 0) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2)";
-        statement = "this animateDoor ['Door_4_source', 1,false];";
+				condition = "(this animationPhase 'Door_4_source' == 0) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2) && ((this getCargoIndex player) != 9) && ((this getCargoIndex player) != 10)";
+				statement = "this animateDoor ['Door_4_source', 1,false];";
 			};
 
 			class close_back_door: door_action {
 				userActionID = 60;
 				displayName = "Close Back Door";
 				available = 1;
-				condition = "(this animationPhase 'Door_4_source' == 1) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2)";
-        statement = "this animateDoor ['Door_4_source', 0,false];";
+				condition = "(this animationPhase 'Door_4_source' == 1) && ((player distance2D (this modelToWorld [0,-3,-1])) < 2) && ((this getCargoIndex player) != 9) && ((this getCargoIndex player) != 10)";
+				statement = "this animateDoor ['Door_4_source', 0,false];";
 			};
 		};
 	};
